9eafe0cf9ddee14978dfbe2379cdaa33c8a90931
Goke Oladokun <gokeladokun@hotmail.com>
2026-09-05 00:16:34 +0100

    Add Web and Hyb

--- BlazorMauiWebServer.AppHost/AppHost.cs
+++ BlazorMauiWebServer.AppHost/AppHost.cs
@@ -2,4 +2,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.GokeWebServer>("gokewebserver");
 
+builder.AddProject<Projects.GokeWeb>("gokeweb");
+
+builder.AddProject<Projects.GokeHyb>("gokehyb");
+
 builder.Build().Run();
---
+++ GokeHyb/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace GokeHyb;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
